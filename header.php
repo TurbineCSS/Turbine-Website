eeef4a32fecf13fa189c5fa5138ad22f17e8cd5b
Peter Kröner <peter@peterkroener.de>
2010-04-29 23:10:32 +0200

    Design for header
    Signed-off-by: Peter Kröner <peter@peterkroener.de>

--- header.php
+++ header.php
@@ -11,17 +11,20 @@
 <div id="wrapper">
 
 
-	<header>
-		<a href="/">
-			<h1><span>Turbine</span></h1>
-		</a>
+	<header>	
+		<h1>
+			<a href="index.php"><span>Turbine</span></a>
+		</h1>
 		<nav>
-			<ul>
-				<li><a href="index.php"><span>Home</span></a></li>
+			<ul id="navi">
+				<li class="first"><a href="index.php"><span>Home</span></a></li>
 				<li><a href="docs.php"><span>Docs</span></a></li>
 				<li><a href="shell.php"><span>Shell</span></a></li>
 				<li><a href="converter.php"><span>Converter</span></a></li>
-				<li><a href="questions.php"><span>Questions?</span></a></li>
+				<li class="last"><a href="questions.php"><span>Questions?</span></a></li>
 			</ul>
 		</nav>
 	</header>
+
+
+	<div class="clear"></div>
