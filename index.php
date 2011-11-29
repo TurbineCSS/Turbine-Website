cbb91618094697bb576d766dfc4dfa0a550491e4
Igor Santos <isantos@i-hunter.net>
2011-11-29 17:18:46 -0200

    reformating index HTML and changing text that say's that turbine is in active development

--- index.php
+++ index.php
@@ -1,30 +1,24 @@
 <?php include('header.php'); ?>
-
-
 <div id="index">
+	<h2>Turbine</h2>
+	<p>
+		Turbine is a collection of PHP-powered tools that are designed to decrease css development time and web developer headache. This includes:
+	</p>
+	<ul>
+		<li>A new, minimal syntax – the less you have have to type, the more you get done</li>
+		<li>Packing, gzipping and automatic minification of multiple style files</li>
+		<li>Constants (also known as "css variables") and selector aliases as well as nested css selectors</li>
+		<li>Oop-like inheritance, extension and templating features</li>
+		<li>Built-in device-, browser- and OS-sniffing</li>
+		<li>Many automatic bugfixes and enhancements for older browsers</li>
+		<li>Fully exensible through a very simple plugin system. A basic understanding of PHP is enough to add completely new features to Turbine</li>
+		<li>A CSS to Turbine converter and a shell app for experiments and development</li>
+	</ul>
 
 
-<h2>Turbine</h2>
-<p>
-	Turbine is a collection of PHP-powered tools that are designed to decrease css development time and web developer headache. This includes:
-</p>
-<ul>
-	<li>A new, minmal syntax – the less you have have to type, the more you get done</li>
-	<li>Packing, gzipping and automatic minification of multiple style files</li>
-	<li>Constants (also known as "css variables") and selector aliases as well as nested css selectors</li>
-	<li>Oop-like inheritance, extension and templating features</li>
-	<li>Built-in device-, browser- and os sniffing</li>
-	<li>Many automatic bugfixes and enhancements for older browsers</li>
-	<li>Fully exensible through a very simple plugin system. A basic understanding of PHP is enough to add completely new features to Turbine</li>
-	<li>A CSS to Turbine converter and a shell app for experiments and development</li>
-</ul>
-
-
-<h3>Example</h3>
-<p>
-	Turbine takes code like this&nbsp;&hellip;
-</p>
-<pre class="turbine">// Welcome to Turbine!
+	<h3>Example</h3>
+	<p>Turbine takes code like this&nbsp;&hellip;</p>
+	<pre class="turbine">// Welcome to Turbine!
 @media screen
 #foo, #bar
     color:#FF0000
@@ -32,10 +26,9 @@
     div.alpha, div.beta
         font-weight:bold
         border-radius:4px</pre>
-<p>
-	&hellip;and turns it into:
-</p>
-<pre class="css">@media screen {
+
+	<p>&hellip;and turns it into:</p>
+	<pre class="css">@media screen {
     #foo, #bar {
         color: #FF0000;
         margin-left: 4px;
@@ -49,23 +42,16 @@
         border-radius: 4px;
     }
 }</pre>
-<p>
-	It is somewhat compareable to <a href="http://sass-lang.com/">Sass</a> and <a href="http://lesscss.org/">Less</a>, but more radically geared towards getting as much done as possible in as few keystrokes as possible.
-</p>
+
+	<p>It is somewhat comparable to <a href="http://sass-lang.com/">Sass</a> and <a href="http://lesscss.org/">Less</a>, but more radically geared towards getting as much done as possible in as few keystrokes as possible.</p>
 
 
-<h3>We need your help!</h3>
-<p>
-	Turbine is still in active development. Help us to make it better!
-</p>
-<ul>
-	<li>Download Turbine, test it and <a href="http://github.com/SirPepe/Turbine/issues">report any bugs you find</a></li>
-	<li><a href="http://github.com/SirPepe/Turbine">Fork it</a> and tackle some <a href="http://github.com/SirPepe/Turbine/issues">open issues</a></li>
-	<li>Add some more <a href="http://github.com/SirPepe/Turbine/blob/master/tests/browser.php">browser test cases</a>
-</ul>
-
-
+	<h3>We need your help!</h3>
+	<p>Turbine is a stable software, but you can help us making it even better!</p>
+	<ul>
+		<li>Download Turbine, test it and <a href="http://github.com/igorsantos07/Turbine/issues">report any bugs you find</a></li>
+		<li><a href="http://github.com/igorsantos07/Turbine">Fork it</a> and tackle some <a href="http://github.com/igorsantos07/Turbine/issues">open issues</a></li>
+		<li>Add some more <a href="http://github.com/igorsantos07/Turbine/blob/master/tests/browser.php">browser test cases</a>
+	</ul>
 </div>
-
-
 <?php include('footer.php'); ?>
