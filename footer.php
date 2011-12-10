0c88d007ec1eacbc6b6a432f7e663b43445963cd
igoru <igorsantos07@gmail.com>
2011-12-10 20:12:44 -0200

    removing old google analytics code and adding getClicky code

--- footer.php
+++ footer.php
@@ -1,27 +1,31 @@
-<div id="footer">
-	<ul>
-		<li><a href="#top">Back to top</a></li>
-		<li><a href="index.php">Home</a></li>
-		<li><a href="http://github.com/SirPepe/Turbine/downloads">Download</a></li>
-		<li><a href="docs.php">Docs</a></li>
-		<li><a href="support.php">Support</a></li>
-		<li><a href="shell.php">Shell</a></li>
-		<li><a href="converter.php">Converter</a></li>
-		<li><a href="http://github.com/SirPepe/Turbine">Development</a></li>
-	</ul>
-</div>
+				<div id="footer">
+					<ul>
+						<li><a href="#top">Back to top</a></li>
+						<li><a href="index.php">Home</a></li>
+						<li><a href="http://github.com/SirPepe/Turbine/downloads">Download</a></li>
+						<li><a href="docs.php">Docs</a></li>
+						<li><a href="support.php">Support</a></li>
+						<li><a href="shell.php">Shell</a></li>
+						<li><a href="converter.php">Converter</a></li>
+						<li><a href="http://github.com/SirPepe/Turbine">Development</a></li>
+					</ul>
+				</div>
+			</div>
+		</div>
 
-
-</div>
-
-
-</div>
-
-
-<script src="http://ajax.googleapis.com/ajax/libs/mootools/1.2.4/mootools-yui-compressed.js"></script>
-<script src="js/mootools-more.js"></script>
-<script src="js/turbine.js"></script>
-
-
+		<script src="http://ajax.googleapis.com/ajax/libs/mootools/1.2.4/mootools-yui-compressed.js"></script>
+		<script src="js/mootools-more.js"></script>
+		<script src="js/turbine.js"></script>
+		<script type="text/javascript">
+			var clicky_site_ids = clicky_site_ids || [];
+			clicky_site_ids.push(66513652);
+			(function() {
+			  var s = document.createElement('script');
+			  s.type = 'text/javascript';
+			  s.async = true;
+			  s.src = '//static.getclicky.com/js';
+			  ( document.getElementsByTagName('head')[0] || document.getElementsByTagName('body')[0] ).appendChild( s );
+			})();
+		</script>
 	</body>
 </html>
