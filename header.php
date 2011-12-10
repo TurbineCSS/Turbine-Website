18e6d7dd43736149aac0519cae0053b46f00caaf
igoru <igorsantos07@gmail.com>
2011-12-10 20:26:40 -0200

    fixing page title and code format

--- header.php
+++ header.php
@@ -2,36 +2,31 @@
 <html>
 	<head>
 		<meta charset="utf-8">
-		<title>Turbine</title>
+		<title>TurbineCSS</title>
 		<link href="turbine/css.php?files=turbine.cssp" rel="stylesheet">
 	</head>
+
 	<body>
+		<div id="top"></div>
 
+		<header>
+			<div class="wrapper">
+				<h1>
+					<a href="index.php"><span>Turbine</span></a>
+				</h1>
+				<nav>
+					<ul id="navi">
+						<li class="first"><a href="index.php"><span>Home</span></a></li>
+						<li><a href="download.php"><span>Download</span></a></li>
+						<li><a href="docs.php"><span>Docs</span></a></li>
+						<li><a href="support.php"><span>Support</span></a></li>
+						<li><a href="shell.php"><span>Sandbox</span></a></li>
+						<li><a href="converter.php"><span>Converter</span></a></li>
+						<li><a href="http://github.com/igorsantos07/Turbine"><span>Development</span></a></li>
+					</ul>
+				</nav>
+			</div>
+		</header>
 
-<div id="top"></div>
-
-
-<header>
-	<div class="wrapper">
-		<h1>
-			<a href="index.php"><span>Turbine</span></a>
-		</h1>
-		<nav>
-			<ul id="navi">
-				<li class="first"><a href="index.php"><span>Home</span></a></li>
-				<li><a href="download.php"><span>Download</span></a></li>
-				<li><a href="docs.php"><span>Docs</span></a></li>
-				<li><a href="support.php"><span>Support</span></a></li>
-				<li><a href="shell.php"><span>Sandbox</span></a></li>
-				<li><a href="converter.php"><span>Converter</span></a></li>
-				<li><a href="http://github.com/igorsantos07/Turbine"><span>Development</span></a></li>
-			</ul>
-		</nav>
-	</div>
-</header>
-
-
-<div id="wrapper">
-
-
-	<div class="clear"></div>
+		<div id="wrapper">
+			<div class="clear"></div>
