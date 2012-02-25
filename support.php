d4fad46eac8137af76f7988f55d8cc1ede8821ff
Igoru <igorsantos07@gmail.com>
2012-02-25 15:04:25 -0200

    * fixing information in support page

--- support.php
+++ support.php
@@ -1,23 +1,18 @@
 <?php include('header.php'); ?>
 
-
 <div id="support">
 
-
 <h2>Support</h2>
-<h3>Have a question?</h3>
+<? /*<h3>Have a question?</h3>
 <p>
 	If you have any questions that <a href="docs.php">the documentation</a> and <a href="docs.php#faq">the FAQ</a> don't answer, check out
 	<a href="http://turbine.qhub.com/">our Qhub page</a>.
-</p>
+</p>*/ ?>
 <h3>Found a bug?</h3>
 <p>
-	To report bugs please use the <a href="http://github.com/SirPepe/Turbine/issues">issue tracker ob Github</a> or send an e-mail to <span class="mail">
-	peter [at] peterkroener [dot] de</span>.
+	To report bugs please use the <a href="http://github.com/igorsantos07/Turbine/issues">issue tracker at Github</a>.
 </p>
 
-
 </div>
-
 
 <?php include('footer.php'); ?>
