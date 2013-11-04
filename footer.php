0363115f01e59e0c97cb2bbebd069b1489f928fb
Igor Santos <igorsantos07@gmail.com>
2013-11-03 22:35:17 -0200

    Changing from GetClicky to Google Analytics

--- footer.php
+++ footer.php
@@ -17,15 +17,12 @@
 		<script src="js/mootools-more.js"></script>
 		<script src="js/turbine.js"></script>
 		<script type="text/javascript">
-			var clicky_site_ids = clicky_site_ids || [];
-			clicky_site_ids.push(66513652);
-			(function() {
-			  var s = document.createElement('script');
-			  s.type = 'text/javascript';
-			  s.async = true;
-			  s.src = '//static.getclicky.com/js';
-			  ( document.getElementsByTagName('head')[0] || document.getElementsByTagName('body')[0] ).appendChild( s );
-			})();
+			(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+				(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+				m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+			})(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+			ga('create', 'UA-26066833-4', 'turbinecss.org');
+			ga('send', 'pageview');
 		</script>
 	</body>
 </html>
